c82fd97cee78365b828ae6c64208e57f12af6d9c
Rodrigo Loureiro <rodrigo@MacBook-Air-de-Rodrigo.local>
2025-11-10 10:26:25 -0300

    fix(utils): corrigir nomes de parâmetros invertidos em increase/decrease

--- src/main/java/com/gildedrose/utils/Utils.java
+++ src/main/java/com/gildedrose/utils/Utils.java
@@ -3,11 +3,11 @@ package com.gildedrose.utils;
 import com.gildedrose.Item;
 
 public class Utils {
-    public static int increase(Item item, int amount, int min) {
-        return Math.min(item.quality + amount, min);
+    public static int increase(Item item, int amount, int max) {
+        return Math.min(item.quality + amount, max);
     }
 
-    public static int decrease(Item item, int amount, int max) {
-        return Math.max(item.quality - amount, max);
+    public static int decrease(Item item, int amount, int min) {
+        return Math.max(item.quality - amount, min);
     }
 }
